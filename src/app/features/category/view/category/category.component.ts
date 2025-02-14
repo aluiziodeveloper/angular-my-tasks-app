2114e2ac846899bcedf6ad4af699ffd55781ec98
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-02-14 17:08:34 -0300

    Categorias

--- src/app/features/category/view/category/category.component.ts
+++ src/app/features/category/view/category/category.component.ts
@@ -1,11 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MainListComponent } from '../../components/main-list/main-list.component';
+import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
+import { CategoryService } from '../../services/category.service';
+import { AsyncPipe } from '@angular/common';
+
+const COMPONENT_IMPORTS = [MainListComponent, ColorsListComponent];
+const PIPE_IMPORTS = [AsyncPipe];
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
-  template: `<p>category works!</p>`,
-  styleUrl: './category.component.css',
+  imports: [...COMPONENT_IMPORTS, ...PIPE_IMPORTS],
+  template: `
+    <div class="flex flex-col justify-between h-full w-full">
+      <!-- main-list -->
+      <app-main-list />
+      <!-- colors-list -->
+      <app-colors-list />
+    </div>`,
+  styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoryComponent { }
+export class CategoryComponent {
+  private readonly categoryService = inject(CategoryService);
+}
